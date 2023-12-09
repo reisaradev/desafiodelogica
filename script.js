2d4db599f9c6801efc339fec35a7a24dd004fcf9
devsara <reisaramaral>
2023-12-08 21:56:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Ano novo está chegando");
+
+let name = "Mariah Carey";
+
+let idade = 54;
+
+let ano = 2023;
+console.log(ano - idade);
+
+const anoNascimento = idade;
+
+console.log(anoNascimento);
+
+let quartaFeira = true;
+
+console.log(quartaFeira);
+
+console.log(typeof quartaFeira);
+
+let _idade = 15;
+
+
+if(_idade >=18) {
+    console.log("Voce é menor de idade podera ir")
+}
+else{
+    console.log("Sinto muito, não poderá ir")
+}
+
+let niver = "Outubro"
+
+if (niver == "Junho" || niver == "Agosto " || niver == "Dezembro") {
+    console.log("Uma ou mais opções estao correts");
+}
+else{
+    console.log(`esta errado, mes digitado foi ${niver}` );
+}
+
+    
+
+
+
+
